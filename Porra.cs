72b2eb80e3eb9354f73f9f59c855fc957e8aa411
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-05-04 10:54:50 +0200

    Azterketa amaituta eta dokumentazioa prest

---
+++ Porra.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3POO_5
+{
+    /// <summary>
+    /// Represents a sweepstake (Porra) containing predictions and accumulated points.
+    /// </summary>
+    public class Porra
+    {
+        private string izena, irabazlea, finalista, goleatzailea;
+        private int puntuak;
+
+        /// <summary>
+        /// Gets or sets the name of the participant or team.
+        /// </summary>
+        public string Izena { get => izena; set => izena = value; }
+
+        /// <summary>
+        /// Gets or sets the predicted winner.
+        /// </summary>
+        public string Irabazlea { get => irabazlea; set => irabazlea = value; }
+
+        /// <summary>
+        /// Gets or sets the predicted finalist or runner-up.
+        /// </summary>
+        public string Finalista { get => finalista; set => finalista = value; }
+
+        /// <summary>
+        /// Gets or sets the predicted goalscorer.
+        /// </summary>
+        public string Goleatzailea { get => goleatzailea; set => goleatzailea = value; }
+
+        /// <summary>
+        /// Gets or sets the accumulated points.
+        /// </summary>
+        public int Puntuak { get => puntuak; set => puntuak = value; }
+
+        /// <summary>
+        /// Initializes a new instance of the Porra class.
+        /// </summary>
+        /// <param name="i">The name of the participant.</param>
+        /// <param name="ir">The predicted winner.</param>
+        /// <param name="fi">The predicted finalist.</param>
+        /// <param name="go">The predicted goalscorer.</param>
+        /// <param name="p">The initial points.</param>
+        public Porra(string i, string ir, string fi, string go, int p)
+        {
+            this.izena = i;
+            this.irabazlea = ir;
+            this.finalista = fi;
+            this.goleatzailea = go;
+            this.puntuak = p;
+        }
+
+        /// <summary>
+        /// Returns a detailed string representation of the sweepstake predictions.
+        /// </summary>
+        /// <returns>A formatted string with all the sweepstake details.</returns>
+        public string Pantailaratu()
+        {
+            return "TALDEA=" + izena + ", bere IRABAZLEA=" + irabazlea + ", beste FINALISTA=" + finalista + ", GOLEATZAILEA=" + goleatzailea + " eta PUNTUAK=" + puntuak;
+        }
+
+        /// <summary>
+        /// Returns a simple string representation of the sweepstake.
+        /// </summary>
+        /// <returns>A string with properties separated by hyphens.</returns>
+        public override string ToString()
+        {
+            return izena + "-" + irabazlea + "-" + finalista + "-" + goleatzailea + "-" + puntuak;
+        }
+
+        /// <summary>
+        /// Calculates the points for correctly guessing the winner.
+        /// </summary>
+        /// <returns>The accumulated points plus 25 bonus points.</returns>
+        public int AsmatuIrabazlea()
+        {
+            return (puntuak + 25);
+        }
+
+        /// <summary>
+        /// Calculates the points for correctly guessing both finalists.
+        /// </summary>
+        /// <returns>The accumulated points plus 20 bonus points.</returns>
+        public int AsmatuBiFinalistak()
+        {
+            return (puntuak + 20);
+        }
+
+        /// <summary>
+        /// Calculates the points for a goal.
+        /// </summary>
+        /// <returns>The accumulated points plus 3 bonus points.</returns>
+        public int Goleko()
+        {
+            return (puntuak + 3);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3POO_5
+{
+    /// <summary>The main class of the application that handles the user interface and data processing.</summary>
+    public class Program
+    {
+        /// <summary>
+        /// A static list containing all the sweepstake (Porra) entries.
+        /// </summary>
+        static List<Porra> lp = new List<Porra>();
+
+        /// <summary>
+        /// Defines the entry point of the application. Displays the main menu and handles user choices.
+        /// </summary>
+        static void Main()
+        {
+            int eragiketa;
+            FitxategiaIrakurri();
+            {
+                do
+                {
+                    Console.WriteLine("***** PORRA ***********");
+                    Console.WriteLine("Ze ariketa nahi duzun egin? Sartu bere zenbakia:");
+                    Console.WriteLine("1.- Egungo porra ikusi");
+                    Console.WriteLine("2.- Emaitzak sartu");
+                    Console.WriteLine("3.- IRTEN");
+                    eragiketa = int.Parse(Console.ReadLine());
+                    switch (eragiketa)
+                    {
+                        case 1:
+                            InprimatuPorrak();
+                            break;
+                        case 2:
+                            SartuEmaitzak();
+                            break;
+                        case 3:
+                            FitxategiraPuntuak();
+                            return;
+                        default:
+                            Console.WriteLine(" *****ERAGIKETA EZ ONARTUA *****");
+                            break;
+                    }
+                } while (eragiketa != 3);
+            }
+        }
+
+        /// <summary>
+        /// Prompts the user to enter the match results (finalists, winner, and goalscorers) and updates the points.
+        /// </summary>
+        static void SartuEmaitzak()
+        {
+            Console.WriteLine("Zein izan dira gaurko bi arerioak, finalistak?");
+            string are1 = Console.ReadLine();
+            string are2 = Console.ReadLine();
+            FinalisteiPuntuEguneraketa(are1, are2);
+            Console.WriteLine("Eta zein izan da irabazlea? Enpate egon bada sakatu intro");
+            string ir = Console.ReadLine();
+            IrabazleeiPuntuEguneraketa(ir);
+            int golkop = lortugolak();
+            for (int i = 1; i <= golkop; i++)
+            {
+                Console.WriteLine("Sartu " + i + ". golaren egilea:");
+                string goleatzaile = Console.ReadLine();
+                GoleatzaileenPuntuEguneraketa(goleatzaile);
+            }
+        }
+
+        /// <summary>
+        /// Asks the user for the number of goals scored and validates the input.
+        /// </summary>
+        /// <returns>
+        /// The valid number of goals entered by the user.
+        /// </returns>
+        static int lortugolak()
+        {
+            while (true)
+            {
+                Console.WriteLine("Zenbat gol egon dira gaur?");
+                try
+                {
+                    int golkop = int.Parse(Console.ReadLine());
+                    return golkop;
+                }
+                catch
+                {
+                    Console.Write("Zenbaki desegokia...");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates the points for participants who correctly guessed the winner.
+        /// </summary>
+        /// <param name="ir">The name of the winning team.</param>
+        static void IrabazleeiPuntuEguneraketa(string ir)
+        {
+            for (int i = 0; i < lp.Count; i++)
+            {
+                if (lp[i].Irabazlea == ir)
+                {
+                    lp[i].Puntuak = lp[i].AsmatuIrabazlea();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates the points for participants who correctly guessed both finalists.
+        /// </summary>
+        /// <param name="fi">The name of the first finalist.</param>
+        /// <param name="ir">The name of the second finalist or winner.</param>
+        static void FinalisteiPuntuEguneraketa(string fi, string ir)
+        {
+            for (int i = 0; i < lp.Count; i++)
+            {
+                if (((lp[i].Finalista == fi) && (lp[i].Irabazlea == ir) || (lp[i].Finalista == ir) && (lp[i].Irabazlea == fi)))
+                {
+                    lp[i].Puntuak = lp[i].AsmatuBiFinalistak();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates the points for participants who correctly guessed a goalscorer.
+        /// </summary>
+        /// <param name="gol">The name of the goalscorer.</param>
+        static void GoleatzaileenPuntuEguneraketa(string gol)
+        {
+            for (int i = 0; i < lp.Count; i++)
+            {
+                if (lp[i].Goleatzailea == gol)
+                {
+                    lp[i].Puntuak = lp[i].Goleko();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads the sweepstake data from the "porrak.txt" file and populates the application's list.
+        /// </summary>
+        static void FitxategiaIrakurri()
+        {
+            StreamReader sr = new StreamReader("porrak.txt");
+            String lerroa;
+            lerroa = sr.ReadLine();
+            while (lerroa != null)
+            {
+                string[] zatitua = lerroa.Split("-");
+                Porra p = new Porra(zatitua[0], zatitua[1], zatitua[2], zatitua[3], int.Parse(zatitua[4]));
+                lp.Add(p);
+                lerroa = sr.ReadLine();
+            }
+            sr.Close();
+
+        }
+
+        /// <summary>
+        /// Saves the updated sweepstake points and data back to the "porrak.txt" file.
+        /// </summary>
+        static void FitxategiraPuntuak()
+        {
+            StreamWriter sw = new StreamWriter("porrak.txt", false);
+            foreach (Porra p in lp)
+            {
+                sw.WriteLine(p.ToString());
+            }
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Prints the current status and points of all sweepstake entries to the console.
+        /// </summary>
+        static void InprimatuPorrak()
+        {
+            Console.WriteLine("************************** EGUNGO PORRAREN EGOERA **********************************");
+            foreach (Porra p in lp)
+            {
+                Console.WriteLine(p.Pantailaratu());
+            }
+            Console.WriteLine("");
+        }
+    }
+}
